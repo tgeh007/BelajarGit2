12715cfa33e25a60f05f9c47c0ebf3f8ea8f61dd
Restu Indrawan <47977088+tgeh007@users.noreply.github.com>
2019-09-29 11:19:22 +0700

    Menambahkan komponen data pada class

--- src/com/puturestu/belajargit/Manusia.java
+++ src/com/puturestu/belajargit/Manusia.java
@@ -1,5 +1,37 @@
 package com.puturestu.belajargit;
 
 public class Manusia {
+    private int umur;
+    private String nama;
+    private String jabatan;
 
+    public int getUmur() {
+        return umur;
+    }
+
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJabatan() {
+        return jabatan;
+    }
+
+    public void setJabatan(String jabatan) {
+        this.jabatan = jabatan;
+    }
+
+    public Manusia(int umur, String nama, String jabatan) {
+        this.umur = umur;
+        this.nama = nama;
+        this.jabatan = jabatan;
+    }
 }
