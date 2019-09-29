ee2e7ce6246d70feafcbea9e480c2619311239d4
Restu Indrawan <47977088+tgeh007@users.noreply.github.com>
2019-09-29 10:56:22 +0700

    Menambahkan variable

--- src/com/puturestu/belajargit/Belajar.java
+++ src/com/puturestu/belajargit/Belajar.java
@@ -2,6 +2,18 @@ package com.puturestu.belajargit;
 
 public class Belajar {
     public static void main(String[] args) {
+        int umur;
+        String nama;
+        String cita;
+
         System.out.println("Belajar Github");
+
+        umur = 17;
+        nama = "Beno";
+        cita = "Coder";
+        System.out.println("Nama: "+nama+"\n" +
+                            "Umur: "+umur+"\n" +
+                            "Cita-cita: "+cita);
+
     }
 }
