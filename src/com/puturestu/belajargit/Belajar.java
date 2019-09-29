707476dfe244986d26139cf9917231f0a06447fa
Restu Indrawan <47977088+tgeh007@users.noreply.github.com>
2019-09-29 11:20:09 +0700

    Menambahkan komponen data pada class

--- src/com/puturestu/belajargit/Belajar.java
+++ src/com/puturestu/belajargit/Belajar.java
@@ -1,19 +1,21 @@
 package com.puturestu.belajargit;
-
+import static com.puturestu.belajargit.Manusia.*;
 public class Belajar {
     public static void main(String[] args) {
         int umur;
         String nama;
         String cita;
+        String jabatan;
 
         System.out.println("Belajar Github");
 
         umur = 17;
         nama = "Beno";
-        cita = "Coder";
-        System.out.println("Nama: "+nama+"\n" +
-                            "Umur: "+umur+"\n" +
-                            "Cita-cita: "+cita);
+        jabatan = "Coder";
 
+        Manusia bejo = new Manusia(umur,nama,jabatan);
+        System.out.println("Nama: "+bejo.getNama()+"\n" +
+                            "Umur: "+bejo.getUmur()+"\n" +
+                            "Cita-cita: "+bejo.getJabatan());
     }
 }
