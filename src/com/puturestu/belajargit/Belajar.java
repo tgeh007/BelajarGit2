df47f968ecabbf4fb071c02f14a2a6ea735c1c4f
Restu Indrawan <47977088+tgeh007@users.noreply.github.com>
2019-09-29 10:44:03 +0700

    Mengubah Tulisan Hallo

--- src/com/puturestu/belajargit/Belajar.java
+++ src/com/puturestu/belajargit/Belajar.java
@@ -2,6 +2,6 @@ package com.puturestu.belajargit;
 
 public class Belajar {
     public static void main(String[] args) {
-        System.out.println("Halo");
+        System.out.println("Belajar Github");
     }
 }
