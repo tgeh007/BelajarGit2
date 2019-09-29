77856bfc0beb80a4a33b470d3374a7d5acd876bc
Restu Indrawan <47977088+tgeh007@users.noreply.github.com>
2019-09-29 10:56:46 +0700

    Menambahkan file baru kucing.java

---
+++ src/com/puturestu/belajargit/Kucing.java
@@ -0,0 +1,12 @@
+package com.puturestu.belajargit;
+
+public class Kucing {
+
+    private static void bicara(){
+        System.out.println("Meoww");
+    }
+
+    public static void main(String[] args) {
+        bicara();
+    }
+}
